a9400d5aba2312cf7e8c6356da896294f932c3f0
HazelC <akinyiagnes.97@gmail.com>
2023-08-01 12:39:46 +0300

    Ft: Add cypress for testing

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: "http://localhost:3000",
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+});
